104d4a026c43ec5e8cf35c1c6d48a375c7acce8f
Yurii Savchuk <geo.savchuk@gmail.com>
2021-01-09 22:14:38 +0200

    Completed progress bar: Event List with pagination and Event Create.

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -46,6 +46,7 @@
 
 <script>
 import Datepicker from 'vuejs-datepicker'
+import NProgress from 'nprogress'
 
 export default {
   components: {
@@ -64,6 +65,8 @@ export default {
   },
   methods: {
     createEvent() {
+      NProgress.start()
+
       this.$store
         .dispatch('event/createEvent', this.event)
         .then(() => {
@@ -73,7 +76,9 @@ export default {
           })
           this.event = this.createFreshEventObject()
         })
-        .catch(() => {})
+        .catch(() => {
+          NProgress.done()
+        })
     },
     createFreshEventObject() {
       const user = this.$store.state.user.user

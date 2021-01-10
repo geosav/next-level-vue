a5e687eec187fd680cd531956c391e83593d3db3
Yurii Savchuk <geo.savchuk@gmail.com>
2021-01-10 16:08:46 +0200

    Added base select component.

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,10 +2,7 @@
   <div>
     <h1>Create an Event</h1>
     <form @submit.prevent="createEvent">
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
-      </select>
+      <BaseSelect label="Select a category" v-model="event.category" :options="categories" />
 
       <h3>Name & describe your event</h3>
       <BaseInput label="Title" v-model="event.title" type="text" placeholder="Add an event title" class="field" />
@@ -23,12 +20,7 @@
         <datepicker v-model="event.date" placeholder="Select a date"/>
       </div>
 
-      <div class="field">
-        <label>Select a time</label>
-        <select v-model="event.time">
-          <option v-for="time in times" :key="time">{{ time }}</option>
-        </select>
-      </div>
+      <BaseSelect label="Select a time" v-model="event.time" :options="times" />
 
       <input type="submit" class="button -fill-gradient" value="Submit"/>
     </form>
@@ -40,9 +32,11 @@
 import Datepicker from 'vuejs-datepicker'
 import NProgress from 'nprogress'
 import BaseInput from "@/components/BaseInput";
+import BaseSelect from "@/components/BaseSelect";
 
 export default {
   components: {
+    BaseSelect,
     BaseInput,
     Datepicker
   },

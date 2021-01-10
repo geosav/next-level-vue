4e4b8a2920bd26249e885c7f29d4de1e2ea24f3d
Yurii Savchuk <geo.savchuk@gmail.com>
2021-01-10 13:37:28 +0200

    Added base input component.

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -8,21 +8,13 @@
       </select>
 
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input v-model="event.title" type="text" placeholder="Add an event title"/>
-      </div>
+      <BaseInput label="Title" v-model="event.title" type="text" placeholder="Add an event title" class="field" />
 
-      <div class="field">
-        <label>Description</label>
-        <input v-model="event.description" type="text" placeholder="Add a description"/>
-      </div>
+      <BaseInput label="Description" v-model="event.description" type="text" placeholder="Add a description" class="field" />
 
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input v-model="event.location" type="text" placeholder="Add a location"/>
-      </div>
+
+      <BaseInput label="Location" v-model="event.location" type="text" placeholder="Add a Location" class="field" />
 
       <h3>When is your event?</h3>
 
@@ -47,9 +39,11 @@
 <script>
 import Datepicker from 'vuejs-datepicker'
 import NProgress from 'nprogress'
+import BaseInput from "@/components/BaseInput";
 
 export default {
   components: {
+    BaseInput,
     Datepicker
   },
   data() {

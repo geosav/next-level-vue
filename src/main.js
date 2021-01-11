674fad622092aefa06e6c1926dfe92ecf7ccf30a
Yurii Savchuk <geo.savchuk@gmail.com>
2021-01-11 10:00:26 +0200

    Added example of form validation with Vuelidate.

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import camelCase from 'lodash/camelCase'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
-import BaseIcon from '@/components/BaseIcon'
+// import BaseIcon from '@/components/BaseIcon'
 import 'nprogress/nprogress.css'
+import Vuelidate from 'vuelidate'
 
-Vue.component('BaseIcon', BaseIcon)
+Vue.use(Vuelidate)
+
+// Vue.component('BaseIcon', BaseIcon)
 
 Vue.config.productionTip = false
 
